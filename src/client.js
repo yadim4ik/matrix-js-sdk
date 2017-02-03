e60840a6e19f9175eb53c73ac28b2239d8e5a20c
Matthew Hodgson <matthew@matrix.org>
2017-02-03 00:40:57 +0000

    track errors when events can't send

--- src/client.js
+++ src/client.js
@@ -963,6 +963,7 @@ function _sendEvent(client, room, event, callback) {
 
         try {
             _updatePendingEventStatus(room, event, EventStatus.NOT_SENT);
+            event.error = err;
 
             if (callback) {
                 callback(err);
